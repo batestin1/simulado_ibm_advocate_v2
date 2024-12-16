740933327195769362e6996ba52c22585db3dd40
batestin1 <mayconcipriano@gmail.com>
2024-12-16 09:10:56 -0300

    v1

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ if "perguntas" not in st.session_state:
         },
 
                 {
-            "pergunta": "Define data centers."
+            "pergunta": "Define data centers.",
             "alternativas": [
                 "a) Centers built in multi-zone regions",
                 "b) Large warehouses containing pods and racks or standardized containers of computing resourcers",
