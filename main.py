452496c75fa8b83ee97def7b6586c86020529459
batestin1 <mayconcipriano@gmail.com>
2024-12-19 09:35:05 -0300

    v1

--- main.py
+++ main.py
@@ -645,7 +645,7 @@ elif pagina == "Responder Simulado":
     with st.form("simulado"):
         respostas = []
         for idx, pergunta in enumerate(st.session_state.perguntas):
-            st.write(f"<h1>{idx + 1}. {pergunta['pergunta']}</h1>")
+            st.write(f"# {idx + 1}. {pergunta['pergunta']}")
             resposta = st.radio(
                 f" ",
                 pergunta["alternativas"],
